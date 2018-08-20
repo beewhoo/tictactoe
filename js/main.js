ac25b8a089ced3d5b832914875684fd2eef899fd
beewhoo <bibeksinghtthapa@gmail.com>
2018-08-20 14:57:34 -0400

    made changes to the js for play again

--- js/main.js
+++ js/main.js
@@ -7,11 +7,12 @@ var winCases = [ [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[0,4,8],[2,4,6], [2,5,8
 var imageTag = document.querySelector('.giphy')
 var winner = document.querySelector('.winner');
 var body = document.querySelector('body');
+var playAgain = document.querySelector('#play-again');
 
 //reset-game
 
 
-var playAgain = document.querySelector('#play-again');
+
 
 playAgain.addEventListener('click', function() {
   var reset = document.querySelectorAll("#box");
@@ -21,6 +22,7 @@ playAgain.addEventListener('click', function() {
 
 
   winner.innerHTML = 'Good Luck!';
+  playAgain.style.display = 'inline-block';
 });
 
 
@@ -40,9 +42,11 @@ function ticTacMove() {
     if (turn%2 != 0){
       this.innerHTML = 'X';
       this.style.color = '#228DFF';
+      playAgain.style.display= 'none';
     } else {
       this.innerHTML = 'O';
       this.style.color = '#FF1177';
+      playAgain.style.display = 'none';
     }
     check();
     turn ++;
@@ -61,8 +65,12 @@ function check() {
     if (foo1 === foo2 && foo2 ===  foo3 && foo1 != "") {
       win = foo1;
       winner.innerHTML = 'PLAYER ' +"'"+ win +"'" + ' WINS!';
-
+      playAgain.style.display = 'inline-block';
     }
+      else if (foo1 !== foo2 && foo2 !== foo3 && foo1 != ""){
+        playAgain.style.display = 'inline-block';
+      }
+
   })
 }
 
